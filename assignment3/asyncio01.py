05c261a46d1092282c439998e5729cc8428d45d0
Your Name <your.email@example.com>
2024-06-28 11:00:16 +0700

    new commit

--- assignment3/asyncio01.py
+++ assignment3/asyncio01.py
@@ -1,1 +1,15 @@
 # check the type of a coroutine
+import asyncio
+# define a coroutine
+async def custom_coro():
+    # await another coroutine
+    await asyncio.sleep(1)
+    
+#create the coroutine
+coro = custom_coro()
+# check the type of the coroutine
+print(type(coro))
+
+#ผลลัพธ์
+#<class 'coroutine'>
+#sys:1: RuntimeWarning: coroutine 'custom_coro' was never awaited
