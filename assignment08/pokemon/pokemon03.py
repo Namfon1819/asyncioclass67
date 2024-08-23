352df47b3ba9da33f3f922511bd6ffe56abbcf0f
Your Name <your.email@example.com>
2024-08-23 09:20:17 +0700

    new commit

--- assignment08/pokemon/pokemon03.py
+++ assignment08/pokemon/pokemon03.py
@@ -5,18 +5,21 @@ import time
 import random
 
 async def get_pokemon(client, url):
-    print(f"{time.ctime()} - get {url}")
+    start_time = time.perf_counter()
+    print(f"{time.ctime()} - Fetching data from {url}")
     resp = await client.get(url)
     pokemon = resp.json()
-
+    end_time = time.perf_counter()
+    
+    duration = end_time - start_time
+    print(f"{time.ctime()} - Fetched data from {url} in {duration:.2f} seconds")
+    
     return Pokemon(pokemon)
 
 async def get_pokemons():
     async with httpx.AsyncClient() as client:
         tasks = []
-        rand_list=[]
-        for i in range(5):
-            rand_list.append(random.randint(1,151))
+        rand_list = [random.randint(1, 151) for _ in range(5)]
 
         for number in rand_list:
             url = f'https://pokeapi.co/api/v2/pokemon/{number}'
@@ -29,7 +32,7 @@ async def index():
     start_time = time.perf_counter()
     pokemons = await get_pokemons()
     end_time = time.perf_counter()
-    print(f"{time.ctime()} - Asynchronous get {len(pokemons)} pokemons. Time taken:{end_time-start_time} seconds")
+    print(f"{time.ctime()} - Asynchronous get {len(pokemons)} pokemons. Time taken: {end_time - start_time:.2f} seconds")
 
 if __name__ == '__main__':
     asyncio.run(index())
