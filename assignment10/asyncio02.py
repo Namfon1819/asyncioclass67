8e30a427dfc68eb7cd248ec28b4663f48b45cf09
Your Name <your.email@example.com>
2024-08-30 10:16:56 +0700

    new comit

--- assignment10/asyncio02.py
+++ assignment10/asyncio02.py
@@ -1,10 +1,11 @@
-# example of using an asyncio queue without blocking
 from random import random
 import asyncio
- 
+import time  # เพิ่มบรรทัดนี้
+
 # coroutine to generate work
 async def producer(queue):
     print('Producer: Running')
+    start_time = time.time()  # เพิ่มบรรทัดนี้
     # generate work
     for i in range(10):
         # generate a value
@@ -19,10 +20,13 @@ async def producer(queue):
     # send an all done signal
     await queue.put(None)
     print('Producer: Done')
+    duration = time.time() - start_time  # เพิ่มบรรทัดนี้
+    print(f'Producer total time: {duration:.2f} seconds')  # เพิ่มบรรทัดนี้
  
 # coroutine to consume work
 async def consumer(queue):
     print('Consumer: Running')
+    start_time = time.time()  # เพิ่มบรรทัดนี้
     # consume work
     while True:
         # get a unit of work without blocking
@@ -39,7 +43,9 @@ async def consumer(queue):
         print(f'\t> Consumer got {item}')
     # all done
     print('Consumer: Done')
- 
+    duration = time.time() - start_time  # เพิ่มบรรทัดนี้
+    print(f'Consumer total time: {duration:.2f} seconds')  # เพิ่มบรรทัดนี้
+
 # entry point coroutine
 async def main():
     # create the shared queue
@@ -49,3 +55,4 @@ async def main():
  
 # start the asyncio program
 asyncio.run(main())
+
